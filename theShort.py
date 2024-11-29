f464585fc9b8588da13d7c69d066b9b857cb4dbc
sun <sun@qq.com>
2024-11-29 23:53:15 +0800

    将军饮马,添加说明

--- theShort.py
+++ theShort.py
@@ -1,40 +1,59 @@
 from manimlib import *
-from RunScene import runScene
 
 
 class TheShort(Scene):
     def construct(self):
         l1 = Line(LEFT_SIDE, end=RIGHT_SIDE)
-        p1 = Dot((-3, 3, 0))
-        p2 = Dot((4, 2, 0))
-        p3 = Dot(p2.get_center() * (1, -1, 0))
-        p4 = Dot((0, 0, 0))
+        pa = Dot((-3, 3, 0))
+        pb = Dot((4, 2, 0))
+        pc = Dot(pb.get_center() * (1, -1, 0))
+        pk = Dot((0, 0, 0))
         route = always_redraw(
             lambda: VGroup(
-                Line(p1.get_center(), p4.get_center(), color=GREEN_D),
+                Line(pa.get_center(), pk.get_center(), color=GREEN_D),
                 DashedLine(
-                    p4.get_center(), p3.get_center(), color=YELLOW_D, dash_length=0.4
+                    pk.get_center(), pc.get_center(), color=YELLOW_D, dash_length=0.4
                 ),
-                Line(p4.get_center(), p2.get_center(), color=YELLOW_D),
+                Line(pk.get_center(), pb.get_center(), color=YELLOW_D),
             )
         )
-        l2 = Line(p1.get_center(), p3.get_center()).set_color(RED_D)
-        Ta = Text("K").next_to(p4, DOWN)
-        for t, p in zip(["A", "B", "C"], [p1, p2, p3]):
+        l2 = Line(pa.get_center(), pc.get_center()).set_color(RED_D)
+        Tk = Text("K").next_to(pk, DOWN)
+        for t, p in zip(["A", "B", "C"], [pa, pb, pc]):
             self.add(Text(t).next_to(p, DOWN))
 
-        self.add(l1, l2, p1, p2, p3, p4, route, Ta)
-        self.add(
-            Text(
-                "将军饮马:KA+KB=KA+KC>AC", font="SimHei", t2c={"将军饮马": RED_D}
-            ).align_on_border(DOWN)
+        self.add(l1, l2, pa, pb, pc, pk, route, Tk)
+        tt = (
+            VGroup(
+                Tex(r"将军饮马:", font_size=48, tex_to_color_map={"将军饮马": RED}),
+                Tex("KA+KB"),
+                Tex("=KA+KC\ge"),
+                Tex(r"AC ="),
+            )
+            .align_on_border(DOWN)
+            .arrange_to_fit_width(9.5)
         )
+        bb = Brace(tt[1], UP)
+        Lac = DecimalNumber(
+            Line(pc.get_center(), pa.get_center()).get_length(), color=RED
+        ).next_to(tt[-1], RIGHT)
+        Lakb = always_redraw(
+            lambda: DecimalNumber(
+                Line(pk.get_center(), pa.get_center()).get_length()
+                + Line(pk.get_center(), pb.get_center()).get_length(),
+                color=RED,
+            ).next_to(bb, UP)
+        )
+
+        self.add(*tt, bb, Lac, Lakb)
         self.play(
-            MoveAlongPath(p4, l1.copy().scale(0.9)),
-            MaintainPositionRelativeTo(Ta, p4),
+            MoveAlongPath(pk, l1.copy().scale(0.9), rate_func=there_and_back),
+            MaintainPositionRelativeTo(Tk, pk),
             run_time=10,
         )
 
 
 if __name__ == "__main__":
+    from RunScene import runScene
+
     runScene(TheShort)
